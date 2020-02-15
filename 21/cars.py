85352dcf99db52cd8ac364c36b260d525e6fdfe2
Robin Beer <r.beer@outlook.de>
2020-02-15 12:37:40 +0100

    solved bite 21

--- 21/cars.py
+++ 21/cars.py
@@ -10,22 +10,33 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    return ", ".join(cars["Jeep"])
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    return [values[0] for values in cars.values()]    
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    
+    list_of_matching_models = []
 
+    for models in cars.values():
+        for model in models:
+            if grep.lower() in model.lower():
+                list_of_matching_models.append(model)
+
+    return sorted(list_of_matching_models)
+
+    # return sorted([model for model in [models for models in cars.values()] 
+    #                 if grep in model.lower()])
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
-       sorted alphabetically"""
-    pass
+       sorted alphabetically""" 
+    
+    return {brand: sorted(models) for brand, models in cars.items()}
