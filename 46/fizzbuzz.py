a1dd05cf051eaa7d752f0f111ceb5106aeb76cb5
Robin Beer <r.beer@outlook.de>
2020-02-15 17:46:34 +0100

    solved 46 without internet

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,2 +1,15 @@
 def fizzbuzz(num):
-    pass
+    
+    if num % 3 == 0 and num % 5 == 0:
+        answer = "Fizz Buzz"
+
+    elif num % 3 == 0 and num % 5 != 0:
+        answer = "Fizz"
+    
+    elif num % 3 != 0 and num % 5 == 0:
+        answer = "Buzz"
+    
+    else:
+        answer = int(num)
+
+    return answer
--- 46/test_fizzbuzz.py
+++ 46/test_fizzbuzz.py
@@ -1,1 +1,17 @@
-# tests are hidden for this Bite
+from fizzbuzz import fizzbuzz
+
+def test_fizzbuzz():
+    input_numbers = range(1, 37)
+
+    expected_string = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz".split(", ") 
+    
+    expected = []
+    for string in expected_string:
+        if string not in ["Fizz", "Buzz", "Fizz Buzz"]:
+            expected.append(int(string))
+        else: 
+            expected.append(string)
+
+    result = [fizzbuzz(number) for number in input_numbers]
+
+    assert result == expected
