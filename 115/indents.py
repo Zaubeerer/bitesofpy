a9097b9b54830e09fc6ab29d5275f26811fc5d7a
Robin Beer <r.beer@outlook.de>
2020-02-19 09:19:04 +0100

    solved bite 115

--- 115/indents.py
+++ 115/indents.py
@@ -1,3 +1,6 @@
 def count_indents(text):
     """Takes a string and counts leading white spaces, return int count"""
-    pass
+    
+    for n_leading_spaces, s in enumerate(text):
+        if s != " ":
+            return n_leading_spaces
