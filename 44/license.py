3630252a9652dc03e414c2948828480604e655c9
Robin Beer <r.beer@outlook.de>
2020-02-19 20:06:23 +0100

    44

--- 44/license.py
+++ 44/license.py
@@ -1,2 +1,9 @@
-def gen_key():
-    pass
+import secrets
+import string
+
+ALPHABET = string.ascii_letters + string.digits
+
+def gen_key(parts=4, chars_per_part=8):
+    key_parts = ["".join([secrets.choice(ALPHABET).upper() for i in range(chars_per_part)])
+                    for _ in range(parts)]
+    return  "-".join(key_parts)
