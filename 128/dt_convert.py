c50ae1b324d4f085d108b84741b273d00b925bc0
Robin Beer <r.beer@outlook.de>
2020-02-19 10:26:51 +0100

    solved bite 128

--- 128/dt_convert.py
+++ 128/dt_convert.py
@@ -9,7 +9,11 @@ def years_ago(date):
        Then extract the year from the obtained datetime object and subtract
        it from the THIS_YEAR constant above, returning the int difference.
        So in this example you would get: 2018 - 2015 = 3"""
-    pass
+    
+
+    extracted_year = datetime.strptime(date, "%d %b, %Y").year
+
+    return THIS_YEAR - extracted_year
 
 
 def convert_eu_to_us_date(date):
@@ -18,4 +22,9 @@ def convert_eu_to_us_date(date):
        To enforce the use of datetime's strptime / strftime (over slicing)
        the tests check if a ValueError is raised for invalid day/month/year
        ranges (no need to code this, datetime does this out of the box)"""
-    pass
+    
+    eu_date = datetime.strptime(date, "%d/%m/%Y")
+
+    us_date = datetime.strftime(eu_date, "%m/%d/%Y")
+
+    return us_date
