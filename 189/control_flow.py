c8211f635575da91772a0934ffff9d7b9a14cc2e
Robin Beer <r.beer@outlook.de>
2020-02-19 19:47:06 +0100

    solved 189

--- 189/control_flow.py
+++ 189/control_flow.py
@@ -4,4 +4,13 @@ MAX_NAMES = 5
 
 
 def filter_names(names):
-    pass
+    n_names = 0
+    for name in names:
+        if not name.startswith(IGNORE_CHAR):
+            if not any(char.isdigit() for char in name):
+                if name.startswith(QUIT_CHAR):
+                    break
+                if n_names == 5:
+                    break
+                n_names += 1
+                yield name
