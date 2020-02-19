c02d871cac05f3de7c1c1a2d24c2856b1b2fdcd8
Robin Beer <r.beer@outlook.de>
2020-02-19 11:07:45 +0100

    solved bite 29 + added pytest.parametrize to test

--- 29/test_wrong_char.py
+++ 29/test_wrong_char.py
@@ -1,17 +1,15 @@
 from wrong_char import get_index_different_char
+import pytest
 
+@pytest.mark.parametrize("input_list, expected", [
+        (['A', 'f', '.', 'Q', 2], 2),
+        (['.', '{', ' ^', '%', 'a'], 4),
+        ([1, '=', 3, 4, 5, 'A', 'b', 'a', 'b', 'c'], 1),
+        (['=', '=', '', '/', '/', 9, ':', ';', '?', '¡'], 5),
+        (list(range(1,9)) + ['}'] + list('abcde'), 8),  # noqa E230
+        ([2, '.', ',', '!'], 0),
+        ])
+def test_wrong_char(input_list, expected):
 
-def test_wrong_char():
-    inputs = (
-        ['A', 'f', '.', 'Q', 2],
-        ['.', '{', ' ^', '%', 'a'],
-        [1, '=', 3, 4, 5, 'A', 'b', 'a', 'b', 'c'],
-        ['=', '=', '', '/', '/', 9, ':', ';', '?', '¡'],
-        list(range(1,9)) + ['}'] + list('abcde'),  # noqa E230
-        [2, '.', ',', '!']
-    )
-    expected = [2, 4, 1, 5, 8, 0]
-
-    for arg, exp in zip(inputs, expected):
-        err = f'get_index_different_char({arg}) should return index {exp}'
-        assert get_index_different_char(arg) == exp, err
+    err = f'get_index_different_char({input_list}) should return index {expected}'
+    assert get_index_different_char(input_list) == expected #, err
--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,2 +1,18 @@
+
+ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+def is_alphanumeric(character):
+    if str(character) == "":
+        return False
+    else:
+        return str(character) in ALPHANUMERIC
+
+
 def get_index_different_char(chars):
-    pass
+
+    list_of_type = [is_alphanumeric(char) for char in chars]
+
+    if sum(list_of_type) > 1:
+        return list_of_type.index(False)
+    else:
+        return list_of_type.index(True)
