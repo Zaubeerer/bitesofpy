a551eceb5dee22e8bdd8977784989ce5fb2ec3dd
Robin Beer <r.beer@outlook.de>
2020-03-02 18:33:23 +0100

    solved 215 (dirty)

--- 215/license_215.py
+++ 215/license_215.py
@@ -5,4 +5,7 @@ def validate_license(key: str) -> bool:
     """Write a regex that matches a PyBites license key
        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
     """
-    pass
+    if len(key) == len("PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4"):
+        return bool(re.match('PB-[A-Z0-9_]{8}-[A-Z0-9_]{8}-[A-Z0-9_]{8}-[A-Z0-9_]{8}', key))
+    else:
+        return False
