13bba849b893453d7ad55c6d564889556ccef1bb
Robin Beer <r.beer@outlook.de>
2020-03-04 17:43:33 +0100

    solved bite 30

--- 30/directors.py
+++ 30/directors.py
@@ -2,6 +2,7 @@ import csv
 from collections import defaultdict, namedtuple
 import os
 from urllib.request import urlretrieve
+import string
 
 BASE_URL = 'https://bites-data.s3.us-east-2.amazonaws.com/'
 TMP = '/tmp'
@@ -22,13 +23,38 @@ def get_movies_by_director():
     """Extracts all movies from csv and stores them in a dict,
     where keys are directors, and values are a list of movies,
     use the defined Movie namedtuple"""
-    pass
+
+    director_dict = defaultdict(list)
+
+    with open(local, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        
+        for row in reader:
+            try:
+                year = int(row['title_year'])
+            except:
+                print("incorrect data format")
+
+            director = row['director_name']
+            title = row['movie_title'].strip(string.whitespace)
+            score = float(row['imdb_score'])
+
+            if year >= MIN_YEAR:
+                movie_tuple = Movie(title, year, score)
+                director_dict[director].append(movie_tuple)
+
+    
+
+    return director_dict
 
 
 def calc_mean_score(movies):
     """Helper method to calculate mean of list of Movie namedtuples,
        round the mean to 1 decimal place"""
-    pass
+    
+    mean_scores = sum(movie.score for movie in movies) / len(movies)
+
+    return round(mean_scores, 1)
 
 
 def get_average_scores(directors):
@@ -36,4 +62,11 @@ def get_average_scores(directors):
        return a list of tuples (director, average_score) ordered by highest
        score in descending order. Only take directors into account
        with >= MIN_MOVIES"""
-    pass
+    
+    director_score = namedtuple('director', 'name score')
+
+    average_scores = [director_score(director, calc_mean_score(movies)) 
+                        for director, movies in directors.items() 
+                        if len(movies) >= MIN_MOVIES]
+
+    return sorted(average_scores, key=lambda director: director.score, reverse=True)
