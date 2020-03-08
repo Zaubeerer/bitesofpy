cead87aa7c7b945086d5fde2288d3756c56c9efd
Robin Beer <r.beer@outlook.de>
2020-03-08 23:29:12 +0100

    solved 66

--- 66/running_mean.py
+++ 66/running_mean.py
@@ -1,5 +1,11 @@
+import itertools
+
+
 def running_mean(sequence):
     """Calculate the running mean of the sequence passed in,
        returns a sequence of same length with the averages.
        You can assume all items in sequence are numeric."""
-    pass
+
+    totals = itertools.accumulate(sequence)
+
+    return [round(total / (i + 1), 2) for i, total in enumerate(totals)]
