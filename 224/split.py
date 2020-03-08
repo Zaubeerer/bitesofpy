ad2c1126a6723d649ebf84c33336c4dc7207f7a3
Robin Beer <r.beer@outlook.de>
2020-03-08 22:12:57 +0100

    solved 224

--- 224/split.py
+++ 224/split.py
@@ -1,4 +1,13 @@
+import re
+
+
 def get_sentences(text):
     """Return a list of sentences as extracted from the text passed in.
        A sentence starts with [A-Z] and ends with [.?!]"""
-    pass
+
+    text = re.sub("\n", " ", text)
+    sentences = re.findall(
+        r"""[A-Z][A-Za-z]*\b\w*\b.+?[.?!] (?![a-z])""", text, re.DOTALL
+    )
+
+    return [sentence.strip() for sentence in sentences]
