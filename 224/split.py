65bb7c6d12973c360c71d02f6eba1e7321cb1ad6
Robin Beer <r.beer@outlook.de>
2020-03-08 22:22:08 +0100

    added re.VERBOSE to 224

--- 224/split.py
+++ 224/split.py
@@ -7,7 +7,14 @@ def get_sentences(text):
 
     text = re.sub("\n", " ", text)
     sentences = re.findall(
-        r"""[A-Z][A-Za-z]*\b\w*\b.+?[.?!] (?![a-z])""", text, re.DOTALL
+        r"""[A-Z]       # A sentences starts with [A-Z]
+        [A-Za-z]*\b     # the subsequent letters should complete the first word
+        \w*\b           # a second word must follow (ending with word end "\b")
+        .+?[.?!]        # the sentence contains at least one more word ".+"
+        \s(?![a-z])     # ends (non-greedy = ?) with a punctuation which is followed by a space (to exclude abbreviations) and a capital letter (not lower case)
+        """,
+        text,
+        re.DOTALL | re.VERBOSE,
     )
 
     return [sentence.strip() for sentence in sentences]
