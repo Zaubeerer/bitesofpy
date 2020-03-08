1ca7e91457e8ebe3425ad469acbb76f5e4a8fe67
Robin Beer <r.beer@outlook.de>
2020-03-08 15:36:50 +0100

    solved 179

--- 179/comments.py
+++ 179/comments.py
@@ -1,3 +1,14 @@
+import re
+import string
+
+string
+
+
 def strip_comments(code):
-    # see Bite description
-    pass
+    # 1. Strip multi-line comments / docstrings
+    stripped_code = re.sub(r"[ ]*([\"]{3}[\n]?)[\s\S]*?([\"]{3}[\n]?)", "", code)
+
+    # 2. Strip one-line / in-line comments
+    stripped_code = re.sub(r"(?!.*[\'\"].*\n)[ ]*#.*?\n", "", stripped_code)
+
+    return stripped_code
