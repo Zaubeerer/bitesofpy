dcaa7469443649168ed3d42435dc0279d5d20c0c
Robin Beer <r.beer@outlook.de>
2020-03-09 00:06:40 +0100

    solved 208

--- 208/combos.py
+++ 208/combos.py
@@ -1,2 +1,11 @@
+from itertools import combinations, permutations
+
+
 def find_number_pairs(numbers, N=10):
-    pass
+    return list(gen_combinations_with_sum_N(numbers, N=N))
+
+
+def gen_combinations_with_sum_N(numbers, N=10):
+    for comb in combinations(numbers, r=2):
+        if sum(comb) == N:
+            yield comb
