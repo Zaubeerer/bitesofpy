9c89e917e2fd8c5db34fcf376a5ce14d69165c09
Robin Beer <r.beer@outlook.de>
2020-03-13 13:42:17 +0100

    solved bite 99 :)

--- 99/sequence_generator.py
+++ 99/sequence_generator.py
@@ -1,12 +1,12 @@
 from itertools import cycle
-from string import ascii_letters
+from string import ascii_uppercase
 
 
 def sequence_generator():
 
     char_list = []
-    for i, letter in enumerate(ascii_letters):
+    for i, letter in enumerate(ascii_uppercase):
         char_list.append(i+1)
         char_list.append(letter.upper())
 
-    return cycle(char_list.copy())
+    return cycle(char_list)
