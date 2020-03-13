9d020899db38b530039c0cc17d094eb76f3e91cb
Robin Beer <r.beer@outlook.de>
2020-03-13 13:33:53 +0100

    solved bite 99 partly

--- 99/sequence_generator.py
+++ 99/sequence_generator.py
@@ -1,2 +1,12 @@
+from itertools import cycle
+from string import ascii_letters
+
+
 def sequence_generator():
-    pass
+
+    char_list = []
+    for i, letter in enumerate(ascii_letters):
+        char_list.append(i+1)
+        char_list.append(letter.upper())
+
+    return cycle(char_list.copy())
