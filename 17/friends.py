2515c54d9721d342a755a4f1c710df8330328642
Robin Beer <r.beer@outlook.de>
2020-03-13 11:41:41 +0100

    solved bite 17 :)

--- 17/friends.py
+++ 17/friends.py
@@ -1,2 +1,9 @@
-def friends_teams():
-    pass
+from itertools import combinations, combinations_with_replacement, permutations
+
+
+def friends_teams(friends, team_size=2, order_does_matter=False):
+
+    if order_does_matter:
+        return permutations(friends, r=team_size)
+    else:
+        return combinations(friends, r=team_size)
