5f1b616b7b4fc578c36720a5335ff9d8d9aad627
Robin Beer <r.beer@outlook.de>
2020-03-13 16:07:25 +0100

    solved 171

--- 171/animation.py
+++ 171/animation.py
@@ -1,6 +1,6 @@
-from itertools import cycle
 import sys
-from time import time, sleep
+from itertools import cycle
+from time import sleep, time
 
 SPINNER_STATES = ['-', '\\', '|', '/']  # had to escape \
 STATE_TRANSITION_TIME = 0.1
@@ -10,8 +10,17 @@ def spinner(seconds):
     """Make a terminal loader/spinner animation using the imports aboveself.
        Takes seconds argument = time for the spinner to runself.
        Does not return anything, only prints to stdout."""
-    pass
+    
+    spinner = cycle(SPINNER_STATES)
 
+    time_limit = time() + seconds
+
+    while time() < time_limit:
+        # TODO: why does flush=True not work?
+        # print(f"\r{next(spinner)}", flush=True)
+        sys.stdout.write(f"\r{next(spinner)}") # no newline
+        sys.stdout.flush()
+        sleep(STATE_TRANSITION_TIME)
 
 if __name__ == '__main__':
     spinner(2)
