284243ead970c63095fa3a8b15cc305d4fa885b0
Robin Beer <r.beer@outlook.de>
2020-03-14 18:00:17 +0100

    solved 67

--- 67/calc_dts.py
+++ 67/calc_dts.py
@@ -7,9 +7,13 @@ pycon_date = date(2018, 5, 8)
 
 def get_hundred_days_end_date():
     """Return a string of yyyy-mm-dd"""
-    pass
+    finish_100days = start_100days + timedelta(days=100)
+
+    return finish_100days.strftime("%Y-%m-%d")
 
 
 def get_days_between_pb_start_first_joint_pycon():
     """Return the int number of days"""
-    pass
+    timedelta = pycon_date - pybites_founded
+
+    return timedelta.days
