ddc91961cf4cc588fbe1f1c3570a51714f4d66be
Robin Beer <r.beer@outlook.de>
2020-03-24 20:52:45 +0100

    solved bite 54

--- 54/poem.py
+++ 54/poem.py
@@ -1,5 +1,20 @@
 INDENTS = 4
 
+import io
+
 
 def print_hanging_indents(poem):
-    pass
+    stripped_lines = [line.strip() for line in poem.split("\n")]
+
+    nice_poem = ""
+    indent = ""
+
+    for line in stripped_lines:
+        if line == "":
+            indent = ""  # eliminate indent, don't print empty line
+        else:
+            nice_poem += f"{indent}{line}\n"
+
+            indent = "    "  # indent
+
+    print(nice_poem)
