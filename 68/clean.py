888db6477952b5edcd544f699345a6260f9d0cb9
Robin Beer <r.beer@outlook.de>
2020-03-25 08:52:25 +0100

    solved 68

--- 68/clean.py
+++ 68/clean.py
@@ -1,3 +1,7 @@
+import re
+import string
+
+
 def remove_punctuation(input_string):
     """Return a str with punctuation chars stripped out"""
-    pass
+    return re.sub(fr"[{string.punctuation}]", "", input_string)
