71f41fb3ae7c501f2ebcaa6da4b4f21b55ac0621
Robin Beer <r.beer@outlook.de>
2020-03-28 20:45:29 +0100

    solved 133

--- 133/affiliation.py
+++ 133/affiliation.py
@@ -1,2 +1,5 @@
-def generate_affiliation_link(url):
-    pass
+def generate_affiliation_link(url, tag="pyb0f-20"):
+
+    url_shortened = url.split("/")
+
+    return f"http://www.amazon.com/{url_shortened[4]}/{url_shortened[5]}/?tag={tag}"
