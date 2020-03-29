c13a8285b1ccda0d62aaa168369b6e4a2b331684
Robin Beer <r.beer@outlook.de>
2020-03-29 13:00:38 +0200

    solved 149

--- 149/words.py
+++ 149/words.py
@@ -3,4 +3,19 @@ def sort_words_case_insensitively(words):
        (1995, 19ab = numbers / Happy, happy4you = strings, hence for
         numbers you only need to check the first char of the word)
     """
-    pass
+    sorted_words = []
+
+    words_starting_with_digits = []
+
+    for word in words:
+        if not word[0].isdigit():
+            sorted_words.append(word)
+        else:
+            words_starting_with_digits.append(word)
+    
+    sorted_words = sorted(sorted_words, key=str.lower)
+
+    for word in sorted(words_starting_with_digits):
+        sorted_words.append(word)
+    
+    return sorted_words
